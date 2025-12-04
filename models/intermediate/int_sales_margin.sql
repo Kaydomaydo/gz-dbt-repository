21fff3e0247f092c4362e576c519c18b3f3c7277
aydoganemre51@gmail.com <aydoganemre51@gmail.com>
2025-12-04 14:47:11 +0000

    sales margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,12 @@
+SELECT
+    s.date_date,
+    s.orders_id,
+    s.products_id,
+    s.revenue,
+    s.quantity,
+    p.purchase_price,
+    ROUND(s.quantity*p.purchase_price) AS purchase_cost,
+    ROUND(s.revenue-p.purchase_price,2) AS margin
+FROM {{ref("stg_raw__sales")}} s 
+LEFT JOIN{{ref("stg_raw__product")}} p 
+    USING(products_id)
